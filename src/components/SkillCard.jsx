986c62b80093a6bd3f2a25411d587ce88f78b6a6
malay77patra <malay77patra@gmail.com>
2026-03-26 13:39:37 +0530

    cleanup

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -14,9 +14,9 @@ export default function Card({ heading = ' ', skills = [], hcolor = 'var(--clr-a
     )
   }
   return (
-    <div className='skill-card' style={{ '--clr': hcolor, color: 'var(--clr)' }}>
+    <div className='skill-card' style={{ '--clr': hcolor }}>
       <h3>
-        <svg width="1.5em" height="1.5em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round">
+        <svg width="1em" height="1em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round">
           <path strokeWidth="3" d="M5.99 6H6m5.99 0H12m5.99 0H18M5.99 12H6m5.99 0H12m5.99 0H18M5.99 18H6m5.99 0H12m5.99 0H18" />
         </svg>
         {heading}
